0aaf304894c1fadec0286adb8bedbcddb3449e6b
peace <git@z7g4n1u8.dev>
2025-11-16 12:32:24 +0530

    Revert "eqe: Remove Firmware Extraction"
    This reverts commit 6eb4f0f201862b7d89d54063ef5752ab2fe15797.

--- extract-files.py
+++ extract-files.py
@@ -86,6 +86,7 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     extract_fns=extract_fns,
+    add_firmware_proprietary_file=True,
     add_generated_carriersettings=True,
 )
 
